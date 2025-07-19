ed79d1f406804a0341dace610e12e6c77d1fc117
sillycarz <doanthetan27@gmail.com>
2025-07-19 15:55:01 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="mindful-bot",
+    version="1.0.0",
+    install_requires=[
+        "discord.py>=2.3.2",
+        "python-dotenv>=1.0.0"
+    ]
+)
